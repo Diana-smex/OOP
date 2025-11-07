2b6f3e5b0a0afaf19363fbf0e0a7b87ec774d20c
Мирхайдарова Диана <lamyrr@bk.ru>
2025-11-07 22:55:45 +0500

    Добавлен класс Line с композицией Point

--- л24.cpp
+++ л24.cpp
@@ -38,3 +38,46 @@ public:
 		cout << "[Point] Точка перемещена на (" << dx << "," << dy << ")" << endl;
 	}
 };
+class Line {
+private:
+	Point start;
+	Point end;
+	string color;
+public:
+	Line() :start(Point(0, 0)), end(Point(1, 1)), color("black") {
+		cout << "[Line] Конструктор по умолчанию вызван" << endl;
+	}
+	Line(const Point& s, const Point& e) : start(s), end(e), color("black") {
+		cout << "[Line] Конструктор с параметрами(две точки) вызван" << endl;
+	}
+	Line(int x1, int y1, int x2, int y2) : start(x1, y1), end(x2, y2), color("black") {
+		cout << "[Line] Конструктор с параметрами ( координаты) вызван" << endl;
+	}
+	Line(const Line& other) : start(other.start), end(other.end), color(other.color) {
+		cout << "[Line] Конструктор копирования вызван" << endl;
+	}
+	~Line() {
+		cout << "[Line] Деструктор вызван" << endl;
+	}
+	void print() const {
+		cout << "[Line] Линия: ";
+		start.print();
+		cout << " -> ";
+		end.print();
+		cout << ", цвет: " << color << endl;
+	}
+	void setColor(const string& newColor) {
+		color = newColor;
+		cout << "[Line] Цвет изменен на: " << color << endl;
+	}
+	void move(int dx, int dy) {
+		cout << "[Line] Перемещаем линию на (" << dx << "," << dy << ")" << endl;
+		start.move(dx, dy);
+		end.move(dx, dy);
+	}
+	double getLength() const {
+		int dx = end.getX() - start.getX();
+		int dy = end.getY() - start.getY();
+		return sqrt(dx * dx + dy * dy);
+	}
+};
